70ab192f5b50ee81f1f1b2f96260d3139ce5e30f
Mario Lazzaroni <mariolazzaroni@MacBook-di-Mario.local>
2026-04-23 11:45:27 +0200

    Changes in beginner workout

--- workouts/beginner_workout.py
+++ workouts/beginner_workout.py
@@ -3,14 +3,20 @@ def running_beginner():
     week = input("Is this your first week of training? (y/n) ")
     while True:
         if week == "y" or "Y":
-            workout_stats = []
             days_xweek = int(input("How many days you can train per week? "))
             hours_xweek = int(input("How many hours you can train per week? "))
-            workout_stats.append(days_xweek, hours_xweek)
-            hours_xday = workout_stats[1] / workout_stats[2]
-            
+            next_week_hours = hours_xweek + 0.10* hours_xweek
+            hours_xtraining = next_week_hours / days_xweek
+            print("Next week you have to train for ", days_xweek, " days, each training long ", hours_xtraining, " hours")
             break
         elif week == "n" or "N":
+            i = 0
+            while i < 2:
+                workout = []
+                workout_stats = int(input("Type Days and hours x week: "))
+                workout.append(workout_stats)
+                
+                
             break
         else:
             print("Please select a valid option!!!")
